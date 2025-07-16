89e5fff03d2e36a700c3ac946dd4bdd09875bff3
v4lue4dded@gmail.com <v4lue4dded@gmail.com>
2025-07-17 00:08:32 +0200

    blub

--- check data.sql
+++ check data.sql
@@ -1,12 +1,55 @@
-select substring(series_id,4,6) as ucc_code, *
+select substring(series_id,4,6) as cx_code
+, 'ucc' as join_type
+, *
 from cx_series
 where lower(series_title) like '%flour%'
 and demographics_code = 'LB01'
 
 
-select substring(series_id,11,6) as eli_code, *
+select
+  substring(series_id || '1', 11, 5) as cu_code_prep,
+  case
+    when cu_code_prep ~ '^[0-9]+$' then cu_code_prep || '0'
+    else cu_code_prep
+  end as cu_code,
+  case
+    when cu_code_prep ~ '^[0-9]+$' then 'ucc'
+    else 'eli'
+  end as join_type,
+  *
 from cu_series
 where lower(series_title) like '%flour%'
+
+
+select substring(series_id,4,6) as cx_code
+, 'ucc' as join_type
+, *
+from cx_series
+where (lower(series_title) like '%cake%'
+or lower(series_title) like '%cookies%'    )
+and demographics_code = 'LB01'
+order by  cx_code
+;
+
+select
+  substring(series_id || '1', 11, 5) as cu_code_prep,
+  case
+    when cu_code_prep ~ '^[0-9]+$' then cu_code_prep || '0'
+    else cu_code_prep
+  end as cu_code,
+  case
+    when cu_code_prep ~ '^[0-9]+$' then 'ucc'
+    else 'eli'
+  end as join_type,
+  *
+from cu_series
+where lower(series_title) like '%cake%'
+   or lower(series_title) like '%cookies%'
+order by cu_code;
+
+;
+
+
 
 select distinct  footnotes
 from series_import
