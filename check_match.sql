7f86861afa18c28c787d774f47722c2dc93c3c73
v4lue4dded@gmail.com <v4lue4dded@gmail.com>
2025-07-17 00:27:09 +0200

    blub

--- check_match.sql
+++ check_match.sql
@@ -48,3 +48,65 @@ where lower(series_title) like '%cake%'
 order by cu_code;
 ;
 
+
+create or replace temp table cx_basis_table as
+select substring(series_id,4,6) as cx_code
+, 'ucc' as join_type
+, *
+from cx_series
+where demographics_code = 'LB01'
+and item_code is null
+
+select
+  cx_code
+, characteristics_code
+, count(*) as freq
+from cx_basis_table as x
+group by
+  cx_code
+, characteristics_code
+order by freq desc
+;
+select *
+from cx_basis_table as x
+
+
+
+select
+  cx_code
+, characteristics_code
+, count(*) as freq
+from cx_basis_table as ba
+full join ce_cpi_concordance as co on ba.cx_code = co.UCC
+group by
+  cx_code
+, characteristics_code
+order by freq desc
+;
+
+select
+  freq
+, count(*) as freqfreq
+from (
+    select
+      cx_code
+    , characteristics_code
+    , count(*) as freq
+    from cx_basis_table as ba
+    full join ce_cpi_concordance as co on ba.cx_code = co.UCC
+    group by
+      cx_code
+    , characteristics_code
+) x
+group by
+  freq
+order by freqfreq desc
+;
+
+
+
+select *
+from cx_basis_table as ba
+full join ce_cpi_concordance as co on ba.cx_code = co.UCC
+where cx_code = '580311'
+and characteristics_code = '01'
